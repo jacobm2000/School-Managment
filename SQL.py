d416ba2fb03e6137a6509a37257e13a7a59642e8
jacobm2000 <maurerja2000@gmail.com>
2023-04-18 13:49:55 -0400

    edited SQL tables
    added a subject table so subjects have unique ids

--- SQL.py
+++ SQL.py
@@ -38,20 +38,35 @@ c.execute("select * from teachers")
 print(c.fetchall())
 conn.commit()
 
-# adding class table
+
+# creates subjects table
+#c.execute("""
+ #         CREATE TABLE subjects(
+  #      subject_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   #     subject_name TEXT
+    #          )
+     #    """)
+     
+#c.execute("INSERT INTO subjects(subject_name) VALUES('Geometry')")
+
+ #adding class table
 #c.execute("""
  #         CREATE TABLE classes(
-  #        class_id INTEGER PRIMARY KEY AUTOINCREMENT,
-   #       class_name TEXT,
+  #       class_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   #       subject_id TEXT,
     #      teacher_id TEXT,
-     ##    ) """)
-#c.execute("INSERT INTO classes(class_name,teacher_id,block) VALUES('Geometry',2,1)")
+     #     block INTEGER
+      #   ) """)
+
+#c.execute("INSERT INTO classes(subject_id,teacher_id,block) VALUES(2,1,1)")
 
 #see all classes
 c.execute("SELECT * FROM classes")
 
 #getting what teachers teach what classes
-c.execute("SELECT classes.class_name,teachers.first,teachers.last FROM classes JOIN teachers ON classes.teacher_id=teachers.teacher_id ")
+c.execute("""SELECT subjects.subject_name,teachers.first,teachers.last FROM classes
+          JOIN teachers ON classes.teacher_id=teachers.teacher_id 
+          JOIN subjects ON classes.subject_id=subjects.subject_id """)
 print(c.fetchall())
 conn.commit()
 conn.close()
